a4b6ee1f51540255fdecca4481ba229c0ba4f500
Faith <infinity1188@yahoo.com>
2017-10-26 01:29:58 +0300

    Groundwork for input

---
+++ DOTNET_CHIP-8/ControlXMLParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DOTNET_CHIP_8
+{
+    public class ControlXMLParser
+    {
+        //returns all bound keyboard
+        public static string[] Binds(string key)
+        {
+            string BindsXML = AppDomain.CurrentDomain.BaseDirectory + @"\data\KeyMappings.xml";
+            List<string> _binds = new List<string>();
+            using (XmlReader reader = XmlReader.Create(BindsXML))
+            {
+                while (reader.Read())
+                {
+                    switch (reader.Name)
+                    {
+                        case "KeyMappings":
+                            break;
+                        case "Bind":
+                            string value = reader["keyboard"];
+                            if (value == key)
+                            {
+                                _binds.Add(reader["console"]);
+                            }
+                            break;
+                    }
+                }
+            }
+            return _binds.ToArray();
+        }
+    }
+}
--- DOTNET_CHIP-8/MainWindow.xaml.cs
+++ DOTNET_CHIP-8/MainWindow.xaml.cs
@@ -6,6 +6,10 @@ using System.Windows.Threading;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Xml;
+using System.Linq;
 
 namespace DOTNET_CHIP_8
 {
@@ -16,38 +20,43 @@ namespace DOTNET_CHIP_8
         DispatcherTimer gfxClock = null;
         DispatcherTimer cpuClock = null;
 
+        private GlobalKeyboardHook KeyboardHook;
+        ObservableCollection<int> Buttons = new ObservableCollection<int>();
+
         string[] Bindings = new string[16];
+
+        private static int[] KeyCodes;
+        private static string[] KeyNames;
 
         public MainWindow()
         {
             InitializeComponent();
+
             AllocConsole();
             Thread.Sleep(5);
 
             CPUCore = new CHIP_8();
-            
-            gfxClock = new DispatcherTimer();
-            gfxClock.Interval = TimeSpan.FromMilliseconds(16.67);
-            gfxClock.Tick += GFX_Tick;
+            InitializeCPUClock();
 
-            cpuClock = new DispatcherTimer();
-            cpuClock.Interval = TimeSpan.FromMilliseconds(1.851851851851852);
-            cpuClock.Tick += CPUCycle;
-
-            Renderer.FrameRendered += DotRenderer_FrameRendered;
-
-            //Dispatcher.Invoke(new Action(() => EmuGrid.Children.Add(Renderer.RenderPort(5))));
-            Dispatcher.Invoke(new Action(() => RenderPort.Children.Add(Renderer.RenderPort(5))));
-
+            InitializeGFXClock();
             Console.WriteLine($"Pixel buffer size: {Renderer.check}");
 
             ReadConfiguration();
 
+            InitializeKeyboardHook();
+            InitializeInputDriver();
         }
 
-        private void DotRenderer_FrameRendered(object sender, EventArgs args)
+        //Window Stuff
+
+        //DLLs for console window
+        [DllImport("Kernel32")]
+        private static extern void AllocConsole();
+        private void OpenConsole()
         {
-            FrameTimerDisplay.Text = $"{Renderer.FrameTime}ms";
+            AllocConsole();
+            Thread.Sleep(1);
+            Console.WriteLine("Booting CHIP-8");
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
@@ -60,6 +69,83 @@ namespace DOTNET_CHIP_8
             LoadNewGame(game);
 
         }
+
+        //Button Region
+
+        private void TestRenderer_Button(object sender, RoutedEventArgs e)
+        {
+            Renderer.TestFillPixels();
+        }
+
+        private void SlowGFXBuffer_Button(object sender, RoutedEventArgs e)
+        {
+            Renderer.SlowFillPixels();
+        }
+
+
+        private void DumpGFXBuffer_Button(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("-----------------------RAW GFX Buffer:");
+            byte[] gfxarray = CPUCore.gfx;
+            foreach (byte px in gfxarray)
+            {
+                Console.Write($"{px}, ");
+            }
+            Console.WriteLine();
+        }
+
+        private void DumpOpCodes_Button(object sender, RoutedEventArgs e)
+        {
+            List<string> log = CPUCore.OpCodeLog;
+
+            StreamWriter writer = new StreamWriter($"{AppDomain.CurrentDomain.BaseDirectory}/opcodes.log");
+            foreach (var entry in log)
+            {
+                writer.WriteLine(entry);
+            }
+            writer.Close();
+            Console.WriteLine($"Written {log.Count} entries to OPCode log");
+        }
+
+        private void DotRenderer_FrameRendered(object sender, EventArgs args)
+        {
+            FrameTimerDisplay.Text = $"{Renderer.FrameTime}ms";
+        }
+
+        //Initializations
+
+        private void InitializeKeyboardHook()
+        {
+            KeyboardHook = new GlobalKeyboardHook();
+            KeyboardHook.KeyboardPressed += On_KeyPressed;
+            Console.WriteLine("Keyboard Hooked");
+        }
+
+        private void InitializeGFXClock()
+        {
+            gfxClock = new DispatcherTimer();
+            gfxClock.Interval = TimeSpan.FromMilliseconds(16.67);
+            gfxClock.Tick += GFX_Tick;
+
+            Dispatcher.Invoke(new Action(() => RenderPort.Children.Add(Renderer.RenderPort(5))));
+
+            Renderer.FrameRendered += DotRenderer_FrameRendered;
+        }
+
+        private void InitializeCPUClock()
+        {
+            cpuClock = new DispatcherTimer();
+            cpuClock.Interval = TimeSpan.FromMilliseconds(1.851851851851852);
+            cpuClock.Tick += CPUCycle;
+        }
+
+        private void InitializeInputDriver()
+        {
+            CreateKeyArrays();
+            Buttons.CollectionChanged += Buttons_Changed;
+        }
+
+        //--
 
         //Load ROM into memory
         private void LoadNewGame(byte[] game)
@@ -113,50 +199,6 @@ namespace DOTNET_CHIP_8
             }
         }
 
-        //DLLs for console window
-        [DllImport("Kernel32")]
-        private static extern void AllocConsole();
-        private void OpenConsole()
-        {
-            AllocConsole();
-            Thread.Sleep(1);
-            Console.WriteLine("Booting CHIP-8");
-        }
-
-        private void DumpGFXBuffer_Button(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("-----------------------RAW GFX Buffer:");
-            byte[] gfxarray = CPUCore.gfx;
-            foreach (byte px in gfxarray)
-            {
-                Console.Write($"{px}, ");
-            }
-            Console.WriteLine();
-        }
-
-        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-
-        }
-
-        private void Window_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
-        {
-
-        }
-
-        private void DumpOpCodes_Button(object sender, RoutedEventArgs e)
-        {
-            List<string> log = CPUCore.OpCodeLog;
-
-            StreamWriter writer = new StreamWriter($"{AppDomain.CurrentDomain.BaseDirectory}/opcodes.log");
-            foreach(var entry in log)
-            {
-                writer.WriteLine(entry);
-            }
-            writer.Close();
-            Console.WriteLine($"Written {log.Count} entries to OPCode log");
-        }
-
         private void ReadConfiguration()
         {
             IniFile config = new IniFile("emuconfig.ini");
@@ -171,14 +213,107 @@ namespace DOTNET_CHIP_8
 
         }
 
-        private void TestRenderer_Button(object sender, RoutedEventArgs e)
+        private void CreateKeyArrays()
         {
-            Renderer.TestFillPixels();
+            string VirtualKeysXML = AppDomain.CurrentDomain.BaseDirectory + @"\data\VirtualKeys.xml";
+
+            List<int> codes = new List<int>();
+            List<string> names = new List<string>();
+
+            using (XmlReader reader = XmlReader.Create(VirtualKeysXML))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "VirtualKeys":
+                                break;
+                            case "Key":
+                                codes.Add(Convert.ToInt32(reader["value"]));
+                                names.Add(reader["equivalent"]);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            KeyCodes = codes.ToArray();
+            KeyNames = names.ToArray();
+            Console.WriteLine($"Added {KeyCodes.Length} virtual keys!");
         }
 
-        private void SlowGFXBuffer_Button(object sender, RoutedEventArgs e)
+        [STAThread]
+        private void On_KeyPressed(object sender, GlobalKeyboardHookEventArgs e)
         {
-            Renderer.SlowFillPixels();
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
+            {
+                if (!Buttons.Contains(e.KeyboardData.VirtualCode))
+                {
+                    Buttons.Add(e.KeyboardData.VirtualCode);
+                }
+
+            }
+            else
+            {
+                Buttons.Remove(e.KeyboardData.VirtualCode);
+            }
+        }
+
+        private void Buttons_Changed(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //= on press
+            if (e.NewItems != null)
+            {
+                foreach (int key in e.NewItems)
+                {
+                    //listen only to supported keys
+                    if (KeyCodes.Contains(key))
+                    {
+                        int loc = Array.IndexOf(KeyCodes, key);
+                        string keyname = KeyNames[loc];
+                        string[] _binds = ControlXMLParser.Binds(keyname);
+
+                        if (_binds.Length >= 0)
+                        {
+                            var i = 0;
+                            foreach (string bind in _binds)
+                            {
+                                ushort _key = (ushort)Convert.ToUInt32(_binds[i]);
+                                CPUCore.PressButton(_key);
+                                i++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            //= on depress
+            if(e.OldItems != null)
+            {
+                foreach (int key in e.OldItems)
+                {
+                    if (KeyCodes.Contains(key))
+                    {
+                        int loc = Array.IndexOf(KeyCodes, key);
+                        string keyname = KeyNames[loc];
+                        string[] _binds = ControlXMLParser.Binds(keyname);
+
+                        if (_binds.Length >= 0)
+                        {
+                            var i = 0;
+                            foreach (string bind in _binds)
+                            {
+                                ushort _key = (ushort)Convert.ToUInt32(_binds[i]);
+                                CPUCore.UnpressButton(_key);
+                                i++;
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
